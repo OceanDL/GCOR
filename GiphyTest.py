4ef8eb969e0c54f08e60c268496c13e682161fda
Seac <oceandouglas.lee@gmail.com>
2018-07-24 09:53:41 -0700

    Saving Gifs

--- GiphyTest.py
+++ GiphyTest.py
@@ -1,3 +1,6 @@
 import urllib.request,json
-data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=ocean&api_key=qstJPgVMjzbl0Mu1yGibicFiubKUFbBQ&limit=5").read())
+search_term = "ocean"
+data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=qstJPgVMjzbl0Mu1yGibicFiubKUFbBQ&limit=1").read())
 print(json.dumps(data, sort_keys=True, indent=4))
+gif_url = data['data'][0]['images']['fixed_height']['url']
+urllib.request.urlretrieve(gif_url, "Gifs/" + search_term + ".gif")
