c7a8c33568e7824f654c29e8b1bfc1527397d598
OceanDL <oceandouglas.lee@calbaptist.edu>
2018-09-14 09:39:21 -0700

    Added EAST Stuff from Adrian at PyimageSearch

--- GiphyTest.py
+++ GiphyTest.py
@@ -3,4 +3,4 @@ search_term = "ocean"
 data = json.loads(urllib.request.urlopen("http://api.giphy.com/v1/gifs/search?q=" + search_term + "&api_key=qstJPgVMjzbl0Mu1yGibicFiubKUFbBQ&limit=1").read())
 print(json.dumps(data, sort_keys=True, indent=4))
 gif_url = data['data'][0]['images']['fixed_height']['url']
-urllib.request.urlretrieve(gif_url, "Gifs/" + search_term + ".gif")
+urllib.request.urlretrieve(gif_url, "gifs/" + search_term + ".mp4")
