dfac681a72fb2f30f636b74245542337922c1858
Christopher Nugent <awedeven+github@gmail.com>
2018-11-23 14:47:57 -0800

    Add useful CLI demo as main for parse.py

--- odst/parse.py
+++ odst/parse.py
@@ -1,6 +1,7 @@
 import os
 from nltk.parse.corenlp import CoreNLPParser
 
+
 class Parser:
 
     def __init__(self):
@@ -10,11 +11,11 @@ class Parser:
     def parse(self, sentence):
         return self.parser.raw_parse(sentence)
 
-# parser = Parser()
-# sentences = parser.parse(("Hello, My name is Melroy."))
-# print(sentences)
 
-# # GUI
-# for line in sentences:
-#     for sentence in line:
-#         sentence.draw()
+if __name__ == '__main__':
+    parser = Parser()
+    sentences = parser.parse((input('Please provide a sentence to parse: ')))
+    for line in sentences:
+        for sentence in line:
+            sentence.draw()
+
