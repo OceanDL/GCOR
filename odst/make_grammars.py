a132091da5ab7f643221ea0a409cc42447eff065
Christopher Nugent <awedeven+github@gmail.com>
2018-11-28 19:14:25 -0800

    Better grammar creation

--- odst/make_grammars.py
+++ odst/make_grammars.py
@@ -1,27 +1,48 @@
 from parse import Parser
 from nltk.tree import Tree
 from nltk.tbl.rule import Rule
+from collections import defaultdict
 
-if __name__ == '__main__':
 
+def make_unfiltered_rules_from_corpus():
     parser = Parser()
-    parsedSents = set()
-    corpusFile = open("../corpus.txt", "r")
-
-    for line in corpusFile.readlines():
-        tree = parser.parse(line)
-        parsedSents.add(tree)
-
+    parsed_sents = set()
+    with open("../corpus.txt", "r") as corpus_file:
+        for line in corpus_file.readlines():
+            tree = parser.parse(line)
+            parsed_sents.add(tree)
     ruleset = set()
-
-    tree: Tree
-    rule: Rule
-    for iterable in parsedSents:
+    for iterable in parsed_sents:
         for tree in iterable:
             for rule in tree.productions():
                 ruleset.add(rule)
+    return '\n'.join([str(r) for r in ruleset])
 
-    grammars = open("../grammars.txt", mode = "r+", encoding = "utf-8")
 
-    for rule in ruleset:
-        grammars.write(rule.__str__() + "\n")
+def trim_nonterminals(rules_as_string):
+    return '\n'.join([r for r in rules_as_string.split('\n') if "'" not in r])
+
+
+def join_productions(rules):
+    productions = defaultdict(list)
+    for rule in rules.splitlines():
+        origin, product = rule.split('->')
+        origin, product = origin.strip(), product.strip()
+        productions[origin].append(product)
+    for k in productions:
+        productions[k].append("'{}'".format(k))
+    return '\n'.join(['{} -> {}'.format(k, ' | '.join(v)) for k, v in productions.items()])
+
+
+if __name__ == '__main__':
+    print('Parsing sentences...')
+    raw_rules = make_unfiltered_rules_from_corpus()
+    print('Filtering rules...')
+    filtered = trim_nonterminals(raw_rules)
+    print('Joining productions...')
+    joined = join_productions(filtered)
+    print('Done!\n')
+    print(joined)
+    with open('../grammar.txt', 'w') as f:
+        f.write(joined)
+
