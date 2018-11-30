4531be1da3c8e742539a28908f52e9c5b8ddf6ce
Christopher Nugent <awedeven+github@gmail.com>
2018-11-30 14:22:15 -0800

    Revert Singleton changes until they are tested

--- odst/parse.py
+++ odst/parse.py
@@ -3,24 +3,9 @@ from nltk.parse.corenlp import CoreNLPParser
 
 class Parser:
 
-	class __Parser:
-        """ Implementation of the singleton class Parser """
-        def __init__(self):
-            self.parser = CoreNLPParser()
-        	self.parser.session.trust_env = False
-
-    # storage for the instance reference
-    __instance = None
-    
     def __init__(self):
-    	# Check whether we already have an instance
-        if Parser.__instance is None:
-            # Create and remember instance
-            Parser.__instance = Parser.__Parser()
-          
-        # Store instance reference as the only member in the handle
-        self.__dict__['_Parser__instance'] = Parser.__instance
-        
+        self.parser = CoreNLPParser()
+        self.parser.session.trust_env = False
 
     def parse(self, sentence):
         return self.parser.raw_parse(sentence)
@@ -32,4 +17,3 @@ if __name__ == '__main__':
     for line in sentences:
         for sentence in line:
             sentence.draw()
-
